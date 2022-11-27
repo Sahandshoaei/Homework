531331cca66005cd5a44176bc763605ffccd26d0
Sahandshoaei <118138717+Sahandshoaei@users.noreply.github.com>
2022-11-28 00:12:38 +0330

    Add files via upload

---
+++ tamrin 5.2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	char a;
+	cout<<"please enter a word:";
+	cin>>a;
+	switch(a){
+		case 'A':
+			cout<<"vowel";
+			break;
+			case 'O':
+				cout<<"vowel";
+				case 'U':
+					cout<<"vowel";
+					break;
+					case 'E':
+						cout<<"vowel";
+						break;
+						case 'I':
+						cout<<"vowel";
+					
+	}
+	getch();
+}
